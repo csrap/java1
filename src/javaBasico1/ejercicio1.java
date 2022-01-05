de0f2863989ff1231cfbcb7f093ac509dfe9a42c
César Pinto <63161873+csrap@users.noreply.github.com>
2022-01-05 16:04:41 +0100

    Ejercicio 1 Java Basico

---
+++ src/javaBasico1/ejercicio1.java
@@ -0,0 +1,28 @@
+package javaBasico1;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        //byte
+        byte valorBytes = 1;
+        System.out.println ( "Valor de byte es:  " + valorBytes +  " => Tipo byte");
+        // int
+        int edad = 13;
+        System.out.println ( "Luis tiene " + edad + " años "+ "=> Tipo int");
+        //long
+        long precio = 50;
+        System.out.println("La sudadera cuesta: " + precio + "€ " + "=> Tipo long");
+        //double
+        double valorPi = 3.1416d;
+        System.out.println("Valor de pin es: " + valorPi + " => Tipo double");
+        //boolean
+        boolean verdadero = true;
+        boolean falso = false;
+        System.out.println("Value is " + verdadero + " => Tipo boolean");
+        System.out.println("It's " + falso + " => Tipo boolean");
+        //String
+        String texto1 = "Aprendiendo Java" + " => Tipo String";
+        System.out.println(texto1);
+
+
+    }
+}
